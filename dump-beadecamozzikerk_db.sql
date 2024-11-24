3ea984e5fef2e0e104a10cf30263750755df6c28
mabeade <milagrosailenwilvers23@gmail.com>
2024-11-24 10:46:04 -0300

    Dump sql

---
+++ dump-beadecamozzikerk_db.sql
@@ -0,0 +1,22 @@
+/*create database BeadeCamozziKerk;*/
+
+use BeadeCamozziKerk_db;
+
+INSERT INTO ciudades (cod_postal, ciudad, provincia) VALUES 
+(3100, 'Paraná', 'Entre Ríos'),
+(3101, 'Colonia Avellaneda', 'Entre Ríos'),
+(3103, 'Crespo', 'Entre Ríos'),
+(3150, 'Victoria', 'Entre Ríos'),
+(3260, 'Concordia', 'Entre Ríos'),
+(3200, 'Concepción del Uruguay', 'Entre Ríos'),
+(3280, 'Gualeguaychú', 'Entre Ríos');
+
+INSERT INTO ciudades (cod_postal, ciudad, provincia) VALUES 
+(3000, 'Santa Fe', 'Santa Fe'),
+(3001, 'Recreo', 'Santa Fe'),
+(3016, 'San José del Rincón', 'Santa Fe'),
+(3040, 'Esperanza', 'Santa Fe'),
+(3080, 'San Carlos', 'Santa Fe'),
+(3105, 'Rafaela', 'Santa Fe'),
+(3560, 'Reconquista', 'Santa Fe'),
+(3550, 'Avellaneda', 'Santa Fe');
